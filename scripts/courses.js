f3ad15d489f3286141d8ef32ccb8bc2fc93f02d6
golrokh <golrokh@stanford.edu>
2018-09-09 07:17:58 +0430

    All

---
+++ scripts/courses.js
@@ -0,0 +1,9 @@
+function toggle() {
+    var image = document.getElementById("buttonimg");
+    var button = document.getElementById("courses");
+    //if (image.src === "images/button-4.png") 
+        image.src = "images/button-3.png";
+    //else 
+      ///  image.src = "images/button-4.png");
+}
+//document.getElementById(theImage).src="http://31.media.tumblr.com/fca646cd8fe87906e605ad7e8d0//39903/tumblr_mmoz4fWT6U1soh1p8o1_500.png");x
